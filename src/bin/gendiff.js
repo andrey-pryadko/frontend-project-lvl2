4c8889336fd81fa3916b53008f51e9925e8b8aca
Andrey Pryadko <andrey@pryadko.com>
2020-04-14 13:25:27 +0300

    help, added options

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -5,6 +5,21 @@ const program = require('commander');
 program
   .version('0.0.50')
   .description('Compares two configuration files and shows a difference.')
-  .parse(process.argv);
+  .option('-f, --format [type]', 'output format')
+  .arguments('<firstConfig> <secondConfig>')
+  // .action(function (cmd, firstConfig, secondConfig) {
+  //   cmdValue = cmd;
+  //   firstConfigValue = firstConfig;
+  //   secondConfigValue = secondConfig;
+  // });
+
+program.parse(process.argv);
+
+if (program.format) console.log('Test format OK');
+
+// if ((typeof firstConfigValue === undefined) ||  (typeof secondConfigValue === undefined)) {
+//   console.error('no config given!');
+//   process.exit(1);
+// }
 
 console.log(program.args);
