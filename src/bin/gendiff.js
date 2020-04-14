2b8b40d3e619e16af7b667986de6d5d383fea46a
Andrey Pryadko <andrey@pryadko.com>
2020-04-14 18:25:49 +0300

    gendiff main logic implemented

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -8,13 +8,39 @@ program
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format [type]', 'output format')
   .arguments('<firstConfig> <secondConfig>')
-  .action(function (firstConfig, secondConfig) {
+  .action((firstConfig, secondConfig) => {
     const firstConfigValue = firstConfig;
     const secondConfigValue = secondConfig;
     const firstData = JSON.parse(fs.readFileSync(firstConfigValue));
     const secondData = JSON.parse(fs.readFileSync(secondConfigValue));
-    console.log(firstData);
-    console.log(secondData);
+    const result = [['\n{']];
+    const tab = '  ';
+    const secondKeys = Object.keys(secondData);
+    const firstKeys = Object.keys(firstData).map((key) => {
+      if (secondKeys.includes(key)) {
+        if (firstData[key] === secondData[key]) {
+          result.push([`${tab}${tab}${key} = ${firstData[key]}`]);
+        } else {
+          result.push([`${tab}- ${key} = ${firstData[key]}`]);
+          result.push([`${tab}+ ${key} = ${secondData[key]}`]);
+        }
+      } else {
+        result.push([`${tab}- ${key} = ${firstData[key]}`]);
+      }
+      return key;
+    });
+
+    secondKeys.map((key) => {
+      if (!firstKeys.includes(key)) {
+        result.push([`${tab}+ ${key} = ${secondData[key]}`]);
+      }
+      return key;
+    });
+
+    result.push(['}']);
+
+    console.log(result.join('\n'));
+
   });
 
 
@@ -23,4 +49,3 @@ program.parse(process.argv);
 if (program.format) console.log('Test format OK');
 
 // console.log(program.args);
-
